3cf8491f02b2f3cc01350904f3faa5bb8d3c9574
barnstorm3r <barnabyc@sky.com>
2016-11-10 20:05:53 +0000

    full box cipher solver

---
+++ box.py
@@ -0,0 +1,38 @@
+cipher = input("Cipher:\n")
+boxnum = int(input("How wide are the boxes?\n--> "))
+boxes = []
+currentstr = ""
+move = []
+rearranged = []
+condensed1 = []
+condensed2 = ""
+
+j = 0
+for i in cipher:
+    j = j + 1
+    currentstr = currentstr + i
+
+    if j == boxnum:
+        boxes.append(currentstr)
+        j = 0
+        currentstr = ""
+
+print(boxes)
+
+for i in range(boxnum):
+    move.append(int(input("Where should character " + str(i+1) + " go?\n--> ")) - 1)
+
+for i in range(len(boxes)):
+    rearranged.append([])
+    for j in range(boxnum):
+        rearranged[i].append("")
+
+    for j in range(boxnum):
+        rearranged[i][move[j]] = boxes[i][j]
+
+for i in range(len(boxes)):
+    condensed1.append("".join(rearranged[i]))
+
+condensed2 = "".join(condensed1)
+
+print("Plaintext: " + condensed2)
