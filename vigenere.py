c825e2c40fc14a13bf45fe374d003c3af3cc6e1f
barnstorm3r <barnabyc@sky.com>
2016-11-24 16:14:42 +0000

    vigenere cipher solver

---
+++ vigenere.py
@@ -0,0 +1,15 @@
+alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+cipher = input("Cipher:\n")
+keyword = input("Keyword in caps:\n-->")
+keywordnums = []
+finished = ""
+
+for char in keyword:
+    keywordnums.append(alphabet.index(char) + 1)
+
+i = 0
+for letter in cipher:
+    finished = finished + alphabet[(alphabet.index(letter) + keywordnums[i])%26]
+    i = i + 1
+    if i == len(keyword):
+        i = 0
