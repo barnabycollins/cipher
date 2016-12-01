462400fb8b1dbcba860fa4e788faf98d507cae3f
barnstorm3r <barnabyc@sky.com>
2016-12-01 21:52:58 +0000

    now supports multicharacter strings

--- letternum.py
+++ letternum.py
@@ -2,7 +2,8 @@ alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
 menu = input("Do you have a:\n1. Letter\n2. Number\n--> ")
 if menu == "1":
     letter = input("\nWhat is the letter?\n--> ")
-    print("The letter is number " + str(alphabet.index(letter)))
+    for i in letter:
+        print("The letter " + i + " is number " + str(alphabet.index(i)))
 
 elif menu == "2":
     number = int(input("\nWhat is the number?\n--> "))
